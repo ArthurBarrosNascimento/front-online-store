2cef6a54ffc2848ac129da70ffea0e343912f1f7
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-08-05 19:21:52 -0300

    Requisito 3 Feito/carrinho-de-compras

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { getCategories } from './services/api';
 import Home from './components/Home';
+import Cart from './components/Cart';
 import './App.css';
 
 class App extends Component {
   render() {
     getCategories().then((categories) => { console.log(categories); });
     return (
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ Cart } />
+        </Switch>
+      </BrowserRouter>
+
     );
   }
 }
