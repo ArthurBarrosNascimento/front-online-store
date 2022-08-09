259d6ee644610b6b01b5e674b96d358df5c5b411
Mariana <nanomarii10@gmail.com>
2022-08-09 19:18:46 -0300

    main-group-13 - adicionando produtos no carrinho

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class Cart extends Component {
+  state = {
+    cartProducts: JSON.parse(localStorage.getItem('cartProducts')),
+    quantyProducts: 0,
+  }
+
+  componentDidMount() {
+    const { cartProducts } = this.state;
+    this.setState({
+      quantyProducts: cartProducts.length,
+    });
+  }
+
   render() {
+    const { cartProducts, quantyProducts } = this.state;
+    const productsList = cartProducts.map((
+      { price, title }, index,
+    ) => (
+      <div key={ index }>
+        <h2 data-testid="shopping-cart-product-name">{ title }</h2>
+        <h2>{ price }</h2>
+      </div>
+    ));
     return (
       <main>
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+        {!cartProducts.length
+          ? (
+            <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+          )
+          : (
+            <div>
+              { productsList }
+              <h2 data-testid="shopping-cart-product-quantity">{ quantyProducts }</h2>
+            </div>
+          )}
       </main>
     );
   }
