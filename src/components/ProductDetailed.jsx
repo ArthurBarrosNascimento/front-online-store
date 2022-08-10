f647a958737f1d6f9e798daefe3e2fb833c19d2e
Ricardo_Souza <rl03souza@gmail.com>
2022-08-10 19:38:00 -0300

    adicionando requisito 09 consertado/10 completo-manipulação de qnt de produtos

--- src/components/ProductDetailed.jsx
+++ src/components/ProductDetailed.jsx
@@ -6,16 +6,52 @@ import { getProductFromId } from '../services/api';
 export default class ProductDetailed extends Component {
   state = {
     product: {},
+    cartProducts: [],
   }
 
   componentDidMount() {
     this.productClicked();
   }
 
+  getLocalStorage = () => {
+    const localStorageProducts = JSON.parse(localStorage.getItem('cartProducts'));
+    const localStorageVerify = !localStorageProducts ? [] : localStorageProducts;
+    this.setState({ cartProducts: localStorageVerify }, () => {
+      const { cartProducts } = this.state;
+      localStorage.setItem('cartProducts', cartProducts);
+    });
+  }
+
   productClicked = async () => {
     const { match: { params: { id } } } = this.props;
     const APIrequest = await getProductFromId(id);
     this.setState({ product: APIrequest });
+  }
+
+  saveOnLocalStorage = (product) => {
+    const xablau = { ...product, quantity: 1 };
+    const { cartProducts } = this.state;
+    const teste = cartProducts.some((cartProduct) => cartProduct.id === product.id);
+    if (teste) {
+      const newCart = cartProducts.map((produto) => {
+        if (produto.id === xablau.id) {
+          return { ...produto, quantity: produto.quantity + 1 };
+        }
+        return produto;
+      });
+
+      this.setState({ cartProducts: newCart }, () => {
+        const { cartProducts: newCartProducts } = this.state;
+        localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
+      });
+    } else {
+      this.setState(() => (
+        { cartProducts: [...cartProducts, xablau] }
+      ), () => {
+        const { cartProducts: newCartProducts } = this.state;
+        localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
+      });
+    }
   }
 
   render() {
@@ -27,6 +63,15 @@ export default class ProductDetailed extends Component {
         <img data-testid="product-detail-image" src={ thumbnail } alt={ title } />
         <h4 data-testid="product-detail-price">{ price }</h4>
         <Link to="/Cart" data-testid="shopping-cart-button">Cart</Link>
+        <div>
+          <button
+            type="button"
+            onClick={ () => this.saveOnLocalStorage({ title, price }) }
+            data-testid="product-detail-add-to-cart"
+          >
+            Adicionar ao carrinho
+          </button>
+        </div>
       </div>
     );
   }
